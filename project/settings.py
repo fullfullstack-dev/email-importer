f6df003660217d06b6164d00a4a730542a680c04
Erik <ericsteinbauer.dev@gmail.com>
2026-01-04 08:04:54 -0600

    Add Django project structure and email data models

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Neo4j optional
 NEO4J_URI = os.getenv("NEO4J_URI", "")
 NEO4J_USER = os.getenv("NEO4J_USER", "")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "")
